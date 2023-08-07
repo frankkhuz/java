a1907509b8bbef14284920dbee4de165e6a82780
Franklin <frankkhuzy@gmail.com>
2023-08-06 18:19:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+Document.querySelector('gradeLetter').textContent =gradeLetter
+Document.querySelector('gradeLetter').textContent =gradeLetter
+Document.querySelector('gradeLetter').textContent =gradeLetter
+Document.querySelector('gradeLetter').textContent =gradeLetter
+
+
+
+
+const score =document.querySelector().value
+ if(score < 40){
+    gradeLetter ="F";
+}else if(score => 40 && score < 45){
+    gradeLetter = "E"
+}
